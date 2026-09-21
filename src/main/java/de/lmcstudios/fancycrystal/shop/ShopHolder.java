6c1c90b5cf752bbc3b79076bca84dbbb511bfa1a
123lumc <123luminecraft@gmail.com>
2026-09-21 20:29:28 +0200

    Create ShopHolder.java

---
+++ src/main/java/de/lmcstudios/fancycrystal/shop/ShopHolder.java
@@ -0,0 +1,9 @@
+package de.lmcstudios.fancycrystal.shop;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class ShopHolder implements InventoryHolder {
+    @Override
+    public Inventory getInventory() { return null; }
+}
