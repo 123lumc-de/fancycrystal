adb111f704aea2c6e8960314f818e5f5126c131f
123lumc <123luminecraft@gmail.com>
2026-09-21 20:30:39 +0200

    Create CrystalsCommand.java

---
+++ src/main/java/de/lmcstudios/fancycrystal/commands/CrystalsCommand.java
@@ -0,0 +1,41 @@
+package de.lmcstudios.fancycrystal.commands;
+
+import de.lmcstudios.fancycrystal.FancyCrystalPlugin;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CrystalsCommand implements CommandExecutor {
+
+    private final FancyCrystalPlugin plugin;
+    private static final MiniMessage MM = MiniMessage.miniMessage();
+
+    public CrystalsCommand(FancyCrystalPlugin plugin) { this.plugin = plugin; }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
+                             @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Nur für Spieler.");
+            return true;
+        }
+        if (!player.hasPermission("fancycrystal.use")) {
+            player.sendMessage(MM.deserialize(plugin.getConfig().getString("messages.prefix", "") +
+                plugin.getConfig().getString("messages.no-permission", "<red>Keine Berechtigung.")));
+            return true;
+        }
+
+        double bal = plugin.getEconomy().getBalance(player);
+        String symbol = plugin.getConfig().getString("currency.symbol", "✦");
+        String msg = plugin.getConfig().getString("messages.balance", "")
+            .replace("%amount%", String.format("%.2f", bal))
+            .replace("%symbol%", symbol);
+
+        player.sendMessage(MM.deserialize(
+            plugin.getConfig().getString("messages.prefix", "") + msg));
+        return true;
+    }
+}
